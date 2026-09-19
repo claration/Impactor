08ebe33e493dbfed453c161984be1794cb2f0457
C <claration@riseup.net>
2026-09-19 16:15:17 -0700

    IDK

--- .cargo/rustc-wrapper.rs
+++ .cargo/rustc-wrapper.rs
@@ -26,11 +26,24 @@ fn find_git_bash() -> PathBuf {
     PathBuf::from("bash")
 }
 
+fn get_script_path() -> PathBuf {
+    if let Ok(mut exe_path) = env::current_exe() {
+        exe_path.pop();
+        let script = exe_path.join("rustc-wrapper.sh");
+        if script.exists() {
+            return script;
+        }
+    }
+    PathBuf::from("./.cargo/rustc-wrapper.sh")
+}
+
 fn main() {
     let bash_path = find_git_bash();
+    let script_path = get_script_path();
     let args: Vec<String> = env::args().skip(1).collect();
+
     let status = Command::new(bash_path)
-        .arg("./.cargo/rustc-wrapper.sh")
+        .arg(script_path)
         .args(&args)
         .status()
         .expect("Failed to execute Git Bash wrapper");
